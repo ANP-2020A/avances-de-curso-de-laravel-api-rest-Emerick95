43c5e8abb0db87682d575a60dd21f58a719aa84b
Emerick95 <ahilton.guaras@epn.edu.ec>
2020-07-15 14:07:09 -0500

    #24 Validation Article

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -9,6 +9,15 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+    private static $rules=[
+        'title' => 'required|string|unique:articles|max:255',
+        'body' => 'required',
+    ];
+
+    private static  $messages = [
+        'required' => 'El campo :attribute es obligatorio.',
+        'body.required' => 'El body no es válido.',
+    ];
     public function index(){
         return new ArticleCollection(Article::paginate(2));
     }
@@ -19,6 +28,11 @@ class ArticleController extends Controller
     }
 
     public function store(Request $request){
+        //Validacion de datos para insertar datos
+
+        $request->validate(self::$rules,self::$messages);
+
+
         $article = Article::create($request->all());
         return response()->json($article, 201);
     }
