55039a1540c703cdabad9387e37ea891002fa6bf
Emerick95 <ahilton.guaras@epn.edu.ec>
2020-07-22 11:06:24 -0500

    #26 Implementacion de imagenes Mediante la API

--- app/Http/Resources/Article.php
+++ app/Http/Resources/Article.php
@@ -24,6 +24,7 @@ class Article extends JsonResource
             'updated_at' => $this->updated_at,
             'user' => "/api/users/". $this->user_id,
             'category' => "/api/users/". $this->category_id,
+            'image'=>$this->image,//para que devuelva la imagen
         ];
     }
 }
